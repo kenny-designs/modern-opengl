c8d1fd686b9f3b7cfd74d16af412c1294a5690be
Alexander Aguilar <alexanderman2004@yahoo.com>
2020-06-05 21:19:09 -0400

    drew a triangle!

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -1,9 +1,144 @@
 #include <stdio.h>
+#include <string.h>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+
+GLuint VAO, VBO, shader;
+
+// Vertex Shader
+static const char* vShader = "                                \n\
+#version 330                                                  \n\
+                                                              \n\
+layout (location = 0) in vec3 pos;                            \n\
+void main()                                                   \n\
+{                                                             \n\
+  gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0f);  \n\
+}";
+
+// Fragment Shader
+static const char* fShader = "                    \n\
+#version 330                                      \n\
+                                                  \n\
+out vec4 color;                                   \n\
+                                                  \n\
+void main()                                       \n\
+{                                                 \n\
+  color = vec4(1.0, 0.0, 0.0, 1.0);               \n\
+}";
+
+
+void CreateTriangle()
+{
+  // define the vertices for the triangle
+  GLfloat vertices[] = {
+    -1.0f, -1.0f, 0.0f,
+     1.0f, -1.0f, 0.0f,
+     0.0f,  1.0f, 0.0f
+  };
+
+  // adds one vertex array to the VRAM and obtain the ID for it
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  // similar to the VAO, we create a single buffer and obtain the ID for it
+  glGenBuffers(1, &VBO);
+  // there are various targets we can bind the VBO to. Here, we'll bind to the
+  // GL_ARRAY_BUFFER 
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  // GL_STATIC_DRAW means that we don't plan on changing the values within the
+  // vertices array. If you do intend to, then use GL_DYNAMIC_DRAW.
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // a good little bit happening here. Refer to the documentation
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(0);
+
+  // Unbind out VBO and VAO by setting to 0
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+}
+
+void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+{
+  GLuint theShader = glCreateShader(shaderType);
+  const GLchar* theCode[1];
+  theCode[0] = shaderCode;
+
+  GLint codeLength[1];
+  codeLength[0] = strlen(shaderCode);
+
+  glShaderSource(theShader, 1, theCode, codeLength);
+  glCompileShader(theShader);
+
+  // tracking errors
+  GLint result = 0;
+  GLchar eLog[1024] = { 0 };
+
+  glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+
+  // if result is 0, then something went wrong!
+  if (!result)
+  {
+    // get the error log
+    glGetShaderInfoLog(theShader, sizeof(eLog), NULL, eLog);
+    printf("Error compiling the %d shader: '%s'\n", shaderType, eLog);
+    return;
+  }
+
+  glAttachShader(theProgram, theShader);
+}
+
+void CompileShader()
+{
+  // creates a new shader program and obtains the ID
+  // remember! These programs live on the VRAM
+  shader = glCreateProgram();
+
+  if (!shader)
+  {
+    printf("Error creating shader program!\n");
+    return;
+  }
+
+  AddShader(shader, vShader, GL_VERTEX_SHADER);
+  AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+  // tracking errors
+  GLint result = 0;
+  GLchar eLog[1024] = { 0 };
+
+  // this will create the executables on the graphics card
+  glLinkProgram(shader);
+  // check to see if everything was linked correctly
+  glGetProgramiv(shader, GL_LINK_STATUS, &result);
+
+  // if result is 0, then something went wrong!
+  if (!result)
+  {
+    // get the error log
+    glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+    printf("Error linking program: '%s'\n", eLog);
+    return;
+  }
+
+  // now we check if the shader works with the current context of OpenGL we're
+  // working with. To do this, we shall validate it!
+  glValidateProgram(shader);
+  glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+
+  // if result is 0, then something went wrong!
+  if (!result)
+  {
+    // get the error log
+    glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+    printf("Error validating program: '%s'\n", eLog);
+    return;
+  }
+}
 
 int main()
 {
@@ -57,6 +192,9 @@ int main()
   // Setup viewport size
   glViewport(0, 0, bufferWidth, bufferHeight);
 
+  CreateTriangle();
+  CompileShader();
+
   // loop until window closed
   while (!glfwWindowShouldClose(mainWindow))
   {
@@ -64,8 +202,22 @@ int main()
     glfwPollEvents();
 
     // Clear window
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // use our shader program
+    glUseProgram(shader);
+
+    // bind our VAO
+    glBindVertexArray(VAO);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // unbind our VAO
+    glBindVertexArray(0);
+
+    // remove our shader program
+    glUseProgram(0);
 
     // There are always two buffers. One that is being drawn to and one that the
     // user can see. Now that we've drawn, do the swap!
